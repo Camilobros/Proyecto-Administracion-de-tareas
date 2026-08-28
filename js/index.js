3d696f4c201530bc24313be4223ee1746c641674
Bryan Camilo Diaz Sanchez <brayancamilodiaz3@gmail.com>
2026-08-28 16:40:16 -0500

    creacion de taskManager

--- js/index.js
+++ js/index.js
@@ -3,41 +3,11 @@ let inputDescripcionjs = document.querySelector('#inputDescripcion');
 let inputfechajs = document.querySelector('#inputfecha')
 let botonCrear = document.querySelector('.btn-primary');
 const botonesCompletar = document.querySelectorAll('.btn-completar');
+const taskManager = new TaskManager();
 
-
-botonesCompletar.forEach(function(boton) {
-    
-    boton.addEventListener('click', function(evento) {
-        
-        evento.preventDefault(); 
-
-        
-        const tarjeta = boton.closest('.card');
-
-        
-        tarjeta.classList.toggle('bg-success-subtle'); // Le pone fondo verde a la tarjeta
-        
-
-        
-        if (tarjeta.classList.contains('bg-success-subtle')) {
-            boton.textContent = 'Pendiente';
-            boton.classList.replace('btn-success', 'btn-warning'); // Botón amarillo
-        } else {
-            boton.textContent = 'Completada';
-            boton.classList.replace('btn-warning', 'btn-success'); // Botón verde
-        }
-    });
-    
-});
-
-
-
-
-
-
-
-botonCrear.addEventListener('click', function (evento) {
-    evento.preventDefault();
+const newTaskForm = document.querySelector('#newTaskForm');
+newTaskForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
     if (inputDescripcionjs.value === "" || inputTitulojs.value === "" || inputfechajs.value === "") {
 
@@ -46,7 +16,7 @@ botonCrear.addEventListener('click', function (evento) {
             width: 600,
             padding: "3em",
             color: "darkorchid",
-            background: "#fff url(/images/trees.png)",
+            background: "#fff",
             backdrop: `
     rgba(0,0,123,0.4)
     url("https://sweetalert2.github.io/images/nyan-cat.gif")
@@ -62,25 +32,57 @@ botonCrear.addEventListener('click', function (evento) {
 
     };
 
-
-
-    let inputDescripcionV = inputDescripcionjs.value;
     let inputTituloV = inputTitulojs.value;
+    let inputDescripcionV = inputDescripcionjs.value;
     let inputfechaV = inputfechajs.value;
 
     let prioridadjs = document.querySelector('input[name="prioridadRadios"]:checked');
-    let prioridadV = prioridadjs.value; 
-
-
-
-
+    let prioridadV = prioridadjs.value;
 
     console.log("El titulo es: " + inputTituloV);
     console.log("La descripcion es: " + inputDescripcionV);
     console.log("la fecha es: " + inputfechaV);
     console.log("La prioridad es: " + prioridadV);
+
+    taskManager.addTask(
+    inputTituloV,
+    inputDescripcionV,
+    inputfechaV,
+    'PORHACER'
+    );
+
+    newTaskForm.reset();
+
+    console.log(taskManager.tasks); 
+
 });
 
-const taskManager = new TaskManager(); 
- 
+
+botonesCompletar.forEach(function (boton) {
+
+    boton.addEventListener('click', function (evento) {
+
+        evento.preventDefault();
+
+
+        const tarjeta = boton.closest('.card');
+
+
+        tarjeta.classList.toggle('bg-success-subtle'); // Le pone fondo verde a la tarjeta
+
+
+
+        if (tarjeta.classList.contains('bg-success-subtle')) {
+            boton.textContent = 'Pendiente';
+            boton.classList.replace('btn-success', 'btn-warning'); // Botón amarillo
+        } else {
+            boton.textContent = 'Completada';
+            boton.classList.replace('btn-warning', 'btn-success'); // Botón verde
+        }
+    });
+
+});
+
+
 console.log(taskManager.tasks); 
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,21 @@
-class TaskManager { 
-    constructor() { 
-        this.tasks = []; 
-    } 
-} 
+class TaskManager {
+    constructor(currentId = 0) {
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+
+    
+    addTask(name, description, dueDate, status) {
+        
+        this.currentId++;
+
+        
+        this.tasks.push({
+            id: this.currentId,
+            name: name,
+            description: description,
+            dueDate: dueDate,
+            status: 'PORHACER' 
+        });
+    }
+}
